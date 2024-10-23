de57080f51f3b3552467564d100010be0b1f4fc0
LAPTOP-FKTATMSD\loren <lorenzobgl@gmail.com>
2024-10-23 11:21:34 +0200

    modified run.sh again

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 echo "Starting mnist.py execution at $(date)" >> log.out
 
 # Execute the Python script directly and ensure logs are redirected to both a log file and stdout/stderr
-python_file="./mnist.py"
+python_file="mnist.py"
 python -u $python_file --model stm --training LifeLong --log 2>&1 | tee -a log.out
