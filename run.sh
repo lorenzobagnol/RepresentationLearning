7621ee8d35badfae0b0104972bd2f0f0e6fc9f76
LAPTOP-FKTATMSD\loren <lorenzobgl@gmail.com>
2025-01-27 12:54:12 +0100

    added nohup to run sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 echo "Starting mnist.py execution at $(date)" >> log.out
 
 # Execute the Python script directly and ensure logs are redirected to both a log file and stdout/stderr
-python -u /workspace/mnist.py --model stm --training LifeLong --log 2>&1 | tee -a log.out
+nohup python -u $python_file --model stm --training LifeLong --log  > log.out 2>&1 
