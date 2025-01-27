a3c15ccee80b5bc28c651a0bee720c6a32df33f3
LAPTOP-FKTATMSD\loren <lorenzobgl@gmail.com>
2025-01-27 13:55:08 +0100

    fixed error

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@
 # Add explicit logging of the starting script
 echo "Starting mnist.py execution at $(date)" >> log.out
 
+
 # Execute the Python script directly and ensure logs are redirected to both a log file and stdout/stderr
+python_file="/workspace/mnist.py"
 nohup python -u $python_file --model stm --training LifeLong --log  > log.out 2>&1 
