fixed loss normalization facto is zero

utils/trainer.py
@@ -280,10 +280,8 @@ class SOMTrainer():
 							target_dist = self.model.target_distance_batch(targets, radius=kwargs["target_radius"])
 							weight_function = torch.mul(neighbourhood_func, target_dist)
 							max_weight_function = torch.max(weight_function,1).values # (batch_size, som_dim)
-							if torch.max(max_weight_function)==0:
-								print("loss normalization zero everywhere")
-								if max(torch.sum(weight_function,1))==0:
-									print("also weight is 0")
+							if torch.min(max_weight_function)==0:
+								print("loss normalization contains zeros.")
 								continue
 							weight_function = torch.div(weight_function, max_weight_function.unsqueeze(1))
 						case "Base-STC":