64870b67e92e19854495b2fb8acec944b3f66b9f
LAPTOP-FKTATMSD\loren <lorenzobgl@gmail.com>
2025-05-02 11:45:38 +0200

    ready for server

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ from utils.config import Config, SOMConfig, PytorchBatchConfig, LifeLongConfig, 
 if __name__ == '__main__':
 
 	# FASHION-MNIST, MNIST, CIFAR10
-	runner=Runner(dataset_name="FASHION-MNIST")
+	runner=Runner(dataset_name="MNIST")
 	
 	# vars0 = [5, 1, 0.2, 0]  # 3 different alpha values
-	vars1 = list(range(1))  # 10 different seeds
-	vars2 = ["Base"]  # different mode values
+	vars1 = list(range(5))  # 10 different seeds
+	vars2 = ["Base"]  
 
 	# Create 9 combinations of alpha and beta values
 	param_combinations = list(product(vars1, vars2))
@@ -42,12 +42,12 @@ if __name__ == '__main__':
 	for (var1, var2) in param_combinations:
 		# Creating a specific config with varying parameters for alpha and var2
 		config = Config(
-			SEED=13,
+			SEED=var1,
 			som_config=SOMConfig(M=20, N=20),
-			LifeLong_config=LifeLongConfig(SIGMA=10, TARGET_RADIUS=1.5, ALPHA=None, BETA=0.02, BATCH_SIZE=2, EPOCHS_PER_SUBSET=2, SUBSET_SIZE=2, DISJOINT_TRAINING=True, LR_GLOBAL_BASELINE=0.1, SIGMA_BASELINE=1.5, LEARNING_RATE=0.1, MODE="Base"),
+			LifeLong_config=LifeLongConfig(SIGMA=10, TARGET_RADIUS=1.5, ALPHA=None, BETA=0.1, BATCH_SIZE=64, EPOCHS_PER_SUBSET=50, SUBSET_SIZE=2, DISJOINT_TRAINING=True, LR_GLOBAL_BASELINE=0.1, SIGMA_BASELINE=1.5, LEARNING_RATE=0.001, MODE="Base"),
 			pytorch_batch_config=PytorchBatchConfig(SIGMA=10, TARGET_RADIUS=1.5, EPOCHS=200, BATCH_SIZE=20, LEARNING_RATE=0.001, BETA=0.01,  MODE="Base"),
 			weights_and_biases_config=WandBConfig(PROJECT="paper"),
-			variables=VARS(SEED=13)
+			variables=VARS(SEED=var1)
 		)
 		runner.run(config)
 
